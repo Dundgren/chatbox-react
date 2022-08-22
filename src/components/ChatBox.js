72e6893b18693510ad038ddb2bfc7836ad9e514b
Dundgren <danne.l.97@hotmail.com>
2022-08-22 17:38:01 +0200

    Finished ChatBox-component

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,85 +1,65 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { apiGetMessages } from "../helpers/message";
 
 export default function ChatBox({username}) {
     // State
-    const [messages, setMessages] = useState(["lol"]);
-    // Functions
-    async function getMessages() {
-        let newMessages = await apiGetMessages();
+    const [messages, setMessages] = useState([]);
+    const [shouldScrollToBottom, setShouldScrollToBottom] = useState(true);
 
-        newMessages.data.forEach(m => {
-            const d = new Date(m.timestamp);
-
-            m.timestamp = d.toLocaleString("sv-SE");
-        });
-
-        setMessages(newMessages.data);
-    }
+    // Refs
+    const chatBoxDiv = useRef(null);
 
     // Effects
-    useEffect(() => {setInterval(getMessages, 1000)}, [getMessages]);
+    useEffect(() => {
+        async function getMessages() {
+            let newMessages = await apiGetMessages();
+    
+            newMessages.data.forEach(m => {
+                const d = new Date(m.timestamp);
+    
+                m.timestamp = d.toLocaleString("sv-SE");
+            });
+    
+            setMessages(newMessages.data);
+        }
+
+        const interval = setInterval(getMessages, 1000);
+
+        return () => {
+            clearInterval(interval);
+        }
+    });
+
+    useEffect(() => {
+        function scrollToBottom() {
+            if (chatBoxDiv.current.scrollHeight - chatBoxDiv.current.scrollTop === chatBoxDiv.current.clientHeight) { // If user has scrolled all the way down turn on auto-scroll
+                setShouldScrollToBottom(true);
+            }
+            if (shouldScrollToBottom) {
+                chatBoxDiv.current.scrollTop = chatBoxDiv.current.scrollHeight;
+            }
+        }
+ 
+        const interval = setInterval(scrollToBottom, 200);
+
+        return () => {
+            clearInterval(interval);
+        }
+    });
 
     // Elements
     const messageElements = messages.map(m => {
-        <h2>{m.username}</h2>
-    })
+        return (
+            <div key={m.id} className={username === m.username ? 'message-box message-right' : 'message-box'}>
+                <h2>{ m.username } <span className="date-string">{ m.timestamp }</span> </h2>
+                <p>{ m.message }</p>
+            </div>
+        );
+    });
 
     return (
-        <div>{messageElements}</div>
+        <div className="chat-box" ref={chatBoxDiv} onScroll={ () => { setShouldScrollToBottom(false) } }>
+            {messageElements}
+        </div>
     )
 }
-
-/* <template>
-    <div id="chat-box" ref="chatBoxDiv" @scroll="this.shouldScrollToBottom = false">
-            <div v-for="m in messages" :key="m.id_" :class="this.$store.state.username == m.username ? 'message-box message-right' : 'message-box'">
-                <h2>{{ m.username }} <span class="date-string">{{ m.timestamp }}</span> </h2>
-                <p>{{ m.message }}</p>
-            </div>
-    </div>
-    <!-- <p v-if="!this.shouldScrollToBottom" id="jump-button"><span>You're viewing old messages. Click here to jump to bottom.</span></p> -->
-</template>
-
-<script>
-import { apiGetMessages } from "../helpers/message";
-
-export default {
-    data() {
-        return {
-            shouldScrollToBottom: true,
-        }
-    },
-    computed: {
-        messages: {
-            get() {
-                return this.$store.state.messages;
-            },
-            set(data) {
-                this.$store.commit("setMessages", data);
-            }
-        }
-    },
-    methods: {
-,
-        scrollToBottom() {
-            if (this.$refs.chatBoxDiv.scrollHeight - this.$refs.chatBoxDiv.scrollTop == this.$refs.chatBoxDiv.clientHeight) {
-                this.shouldScrollToBottom = true;
-            }
-            if (this.shouldScrollToBottom) {
-                this.$refs.chatBoxDiv.scrollTop = this.$refs.chatBoxDiv.scrollHeight;
-            }
-        },
-
-    },
-    created() {
-        
-    },
-    mounted() {
-        setInterval(this.scrollToBottom, 200);
-    }
-}
-</script>
-
-<style lang="scss" scoped>
-
-</style> */
